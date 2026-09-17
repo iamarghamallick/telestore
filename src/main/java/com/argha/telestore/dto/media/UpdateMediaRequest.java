ccae4c03ec879067b780423077fe545b6aa1bb99
Argha Mallick <arghamallick5@gmail.com>
2026-09-17 19:22:11 +0530

    minor fixes

--- src/main/java/com/argha/telestore/dto/media/UpdateMediaRequest.java
+++ src/main/java/com/argha/telestore/dto/media/UpdateMediaRequest.java
@@ -1,6 +1,5 @@
 package com.argha.telestore.dto.media;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -8,6 +7,5 @@ public class UpdateMediaRequest {
 
     private String folderId;
 
-    @NotBlank(message = "File name is required")
     private String filename;
 }
